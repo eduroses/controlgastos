3df38f88dc5a4bcbb3acb59c2ab25d209774a0fd
eduroses <edu.s.roses@gmail.com>
2026-08-24 15:55:08 -0300

    Add Supabase client configuration

---
+++ src/supabaseClient.js
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js';
+const SUPABASE_URL = 'https://jrkfnbgiafwkdpiwylis.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_3Rs7NWY_fl2DLYqbhEs5Sw_TONFx8N4';
+export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
